ebce2b9ebf18441da376da36e7c91e2237fe6154
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2023-12-31 13:50:21 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,27 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		int N = Integer.parseInt(args[0]);
+		int J = 0;
+
+			for ( int i = 1 ; i <= N; i++){
+				if(N % i == 0){
+					J=J+i;
+				}else{
+					J = J + 0;
+				}		
+			}
+			if (J - N == N){
+				System.out.print(N + " is a perfect number since " + N + " = 1");
+				for (int D = 2; D < N; D++){
+					if (N % D == 0){
+						System.out.print(" + " + D);
+					} 
+				}
+					System.out.println();	
+				
+			}else{
+				System.out.print(N + " is not a perfect number");
+			}
+		}
 }
