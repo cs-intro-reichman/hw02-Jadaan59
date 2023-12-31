21771e290877d43362516b8c6cf8c226fb9e21f6
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2023-12-31 16:36:14 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,14 +3,14 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
+		int n = 6;
 
 		for (int i = 1; i <= n; i++){
 				if (i%2 == 0){
 					System.out.print(" ");
 				}	
 			for (int j = 1; j <= n; j++){
-					System.out.print("* ");				
+					System.out.print(" *");				
 			}
 					System.out.println();
 		}
